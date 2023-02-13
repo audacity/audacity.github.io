ec9d7cd06711ee3b0a28cc886ab5144b549812dd
Dilson's Pickles <a.dawson@mu.se>
2023-02-13 18:45:11 +0000

    Create release data file

---
+++ src/assets/js/releaseData.js
@@ -0,0 +1,42 @@
+export const releaseData = {
+  win: {
+    x32: {
+        name: "audacity-win-3.2.4-x32.exe",
+        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x32.exe"
+    },
+    x32_zip: {
+        name: "audacity-win-3.2.4-x32.zip",
+        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x32.zip"
+    },
+    x64: {
+        name: "audacity-win-3.2.4-x64.exe",
+        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x64.exe"
+    },
+    x64_zip: {
+        name: "audacity-win-3.2.4-x64.zip",
+        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x64.zip"
+    }
+  },
+  mac: {
+    arm64: {
+      name: "audacity-macOS-3.2.4-arm64.dmg",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-macOS-3.2.4-arm64.dmg",
+    },
+    universal: {
+      name: "audacity-macOS-3.2.4-universal.dmg",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-macOS-3.2.4-universal.dmg",
+    },
+    x86: {
+      name: "audacity-macOS-3.2.4-x86_64.dmg",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-macOS-3.2.4-x86_64.dmg",
+    },
+  },
+  linux: {
+    name: "audacity-linux-3.2.4-x64.AppImage",
+    browser_download_url:
+      "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-linux-3.2.4-x64.AppImage",
+  },
+};
