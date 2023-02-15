dcc08bd9e0e882f7cef5f9c323088e2bd4300860
Dilson's Pickles <a.dawson@mu.se>
2023-02-15 11:59:04 +0000

    Add hamburger menu button

--- src/components/NavigationReact.jsx
+++ src/components/NavigationReact.jsx
@@ -9,8 +9,8 @@ function NavigationReact() {
   }
 
   return (
-    <nav className="relative bg-white border-b-2 z-50">
-      <div className="flex justify-between mx-w-sm mx-auto py-3 px-4">
+    <nav className="relative bg-white border-b-2">
+      <div className="flex justify-between mx-w-sm mx-auto h-12 px-4 items-center">
         <a href="/">
           <span className="text-blue-700 text-base font-bold">Audacity</span>
         </a>
@@ -54,11 +54,17 @@ function NavigationReact() {
             </a>
           </li>
         </ul>
-        <button className="sm:hidden" onClick={() => navButtonHandler()}>
-          Menu
+        <button className="flex w-10 h-10 justify-center items-center sm:hidden" onClick={() => navButtonHandler()}>
+          <svg
+            className="fill-gray-700 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+          </svg>
         </button>
       </div>
-      <AnimatePresence mode="wait">
+      <AnimatePresence>
         {isOpen && (
           <motion.div
             initial={{ height: 0 }}
