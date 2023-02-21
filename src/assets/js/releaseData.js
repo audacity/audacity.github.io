593ee5c6ec453bd0483980ab49544b7644a5809f
Dilson's Pickles <a.dawson@mu.se>
2023-02-21 13:36:33 +0000

    Create download cards

--- src/assets/js/releaseData.js
+++ src/assets/js/releaseData.js
@@ -1,22 +1,38 @@
 export const releaseData = {
-  win: {
-    x32: {
-        name: "audacity-win-3.2.4-x32.exe",
-        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x32.exe"
+  win: [
+    {
+      name: "32 bit installer",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x32.exe",
+      checksum:
+        "047de5ea5104c90567fcffb241f713504cfe520e0baeda63054d55030aa5c916",
+      type: ".exe",
     },
-    x32_zip: {
-        name: "audacity-win-3.2.4-x32.zip",
-        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x32.zip"
+    {
+      name: "32 bit zip file",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x32.zip",
+      checksum:
+        "0e1f487da943962c4f7546d25119ce4e9177917bf50267e951efb19c2a22b311",
+      type: ".zip",
     },
-    x64: {
-        name: "audacity-win-3.2.4-x64.exe",
-        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x64.exe"
+    {
+      name: "64 bit installer",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x64.exe",
+      checksum:
+        "82762426cd3256d30d065b3f99d2f4e20e5ad1944cf25ed4c094ad2690c9b730",
+      type: ".exe",
     },
-    x64_zip: {
-        name: "audacity-win-3.2.4-x64.zip",
-        browser_download_url: "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x64.zip"
-    }
-  },
+    {
+      name: "64 bit zip file",
+      browser_download_url:
+        "https://github.com/audacity/audacity/releases/download/Audacity-3.2.4/audacity-win-3.2.4-x64.zip",
+      checksum:
+        "ade12fdc9bda6d5fe39f2313102387ecdd7194e6b67b50decd53f8c80fa69ea7",
+      type: ".zip",
+    },
+  ],
   mac: {
     arm64: {
       name: "audacity-macOS-3.2.4-arm64.dmg",
---
+++ src/components/card/DownloadCard.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+function DownloadCard(props) {
+  const { title, checksum, downloadURL, buttonText, downloadType } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  function showDetailsHandler() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className="border rounded-md p-4 flex flex-col">
+      <div className="flex justify-between underline-offset-2">
+        <h4 className="text-md sm:text-lg font-bold">{title}</h4>
+
+        {isOpen ? (
+          <div class="flex gap-x-2">
+            <div
+              onClick={() => showDetailsHandler()}
+              className="flex space-x-2 items-center"
+            >
+              <p className="text-sm sm:text-base">Hide details</p>
+              <svg
+                className="w-3 sm:w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M201.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 173.3 54.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
+              </svg>
+            </div>
+          </div>
+        ) : (
+          <div class="flex gap-x-2">
+            <div
+              onClick={() => showDetailsHandler()}
+              className="flex space-x-2 items-center"
+            >
+              <p className="text-sm sm:text-base">Show details</p>
+              <svg
+                className="w-3 sm:w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+              </svg>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            className="mt-4"
+          >
+            <label>Checksum</label>
+            <div className="p-4 border bg-gray-50 mt-2">
+              <p className="font-mono text-sm break-words">{checksum}</p>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <a
+        href={downloadURL}
+        download
+        className="flex items-center mt-6 h-10 w-fit px-3 bg-blue-700 hover:bg-blue-600 text-sm sm:text-base text-white rounded"
+      >
+        {`${buttonText}` + `${downloadType}`}
+      </a>
+    </div>
+  );
+}
+
+export default DownloadCard;
