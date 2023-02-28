bf226599b9fd359237210c6b56a7741266132d33
Dilson's Pickles <a.dawson@mu.se>
2023-02-28 15:13:55 +0000

    Correct class to className

--- src/components/button/DownloadsPaginationButton.jsx
+++ src/components/button/DownloadsPaginationButton.jsx
@@ -5,7 +5,7 @@ function DownloadsPaginationButton(props) {
   return (
     <a href={targetUrl} class="flex flex-1 flex-col border rounded p-4 hover:bg-gray-50">
       <p>Download for</p>
-      <div class="flex items-center text-lg lg:text-xl font-bold gap-x-2 text-gray-800 fill-gray-500">
+      <div className="flex items-center text-lg lg:text-xl font-bold gap-x-2 text-gray-800 fill-gray-500">
         {children}
       </div>
     </a>
