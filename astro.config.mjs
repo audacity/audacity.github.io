6428bd24067107cea0a92c52269bbbab2e7ca6a6
Dilson's Pickles <a.dawson@mu.se>
2023-03-07 09:34:00 +0000

    Change config URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dawsoncustonscole.github.io',
+  site: 'https://audacity-website.github.io',
   integrations: [tailwind(), react()]
 });
