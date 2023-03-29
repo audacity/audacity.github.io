6b14b5a524f2a86f1295f60ec61f6b7b5abd9a39
Dilson's Pickles <a.dawson@mu.se>
2023-03-29 16:47:50 +0100

    Add flex grow

--- src/components/button/DownloadMuseHubButton.jsx
+++ src/components/button/DownloadMuseHubButton.jsx
@@ -11,7 +11,7 @@ function DownloadMuseHubButton(props) {
   function renderButton(href) {
     return (
       <a
-        className="flex flex-col gap-1 py-2 px-4 items-center rounded justify-center border border-gray-200 text-blue-700 hover:bg-gray-100 hover:border-gray-300"
+        className="flex flex-col flex-grow gap-1 py-3 px-4 items-center rounded justify-center border border-gray-200 text-blue-700 hover:bg-gray-100 hover:border-gray-300"
         href={href}
       >
         Audacity + free effects & samples
