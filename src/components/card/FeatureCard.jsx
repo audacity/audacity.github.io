ec2d9438f2681b42ac6b3073831ce0ce7033579a
Dilson's Pickles <a.dawson@mu.se>
2023-08-17 14:49:59 +1000

    Improve text sizing

--- src/components/card/FeatureCard.jsx
+++ src/components/card/FeatureCard.jsx
@@ -4,10 +4,10 @@ function FeatureCard(props) {
   const { children, title, description } = props;
 
   return (
-    <div class="col-span-3 p-6 border drop-shadow-lg bg-white rounded-lg h-96 flex flex-col gap-3">
+    <div class="col-span-3 p-6 border drop-shadow-lg bg-white rounded-lg h-96 flex flex-col">
       {children}
-      <h3 class="text-2xl font-bold">{title}</h3>
-      <p class="text-xl text-gray-600">{description}</p>
+      <h3 class="text-xl mt-3 font-bold">{title}</h3>
+      <p class="mt-2 text-lg text-gray-600">{description}</p>
     </div>
   );
 }
