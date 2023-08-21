ff6292b64be7512574351ab6616d54be02b47785
Dilson's Pickles <a.dawson@mu.se>
2023-08-21 13:38:32 +1000

    Update download for windows

--- src/components/card/DownloadCard.jsx
+++ src/components/card/DownloadCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 function DownloadCard(props) {
-  const { title, checksum, downloadURL, buttonText, downloadType } = props;
+  const { title, checksum, buttonText, downloadURL, downloadType } = props;
   const [isOpen, setIsOpen] = useState(false);
 
   function showDetailsHandler() {
@@ -10,44 +10,29 @@ function DownloadCard(props) {
   }
 
   return (
-    <div className="border rounded-md p-4 flex flex-col">
-      <div className="flex justify-between underline-offset-2">
-        <h4 className="text-md sm:text-lg font-bold">{title}</h4>
-
-        {isOpen ? (
-          <div className="flex gap-x-2">
-            <div
-              onClick={() => showDetailsHandler()}
-              className="flex space-x-2 items-center"
-            >
-              <p className="text-sm sm:text-base">Hide details</p>
-              <svg
-                className="w-3 sm:w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path d="M201.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 173.3 54.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-              </svg>
-            </div>
-          </div>
-        ) : (
-          <div className="flex gap-x-2">
-            <div
-              onClick={() => showDetailsHandler()}
-              className="flex space-x-2 items-center"
-            >
-              <p className="text-sm sm:text-base">Show details</p>
-              <svg
-                className="w-3 sm:w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
-              </svg>
-            </div>
-          </div>
-        )}
+    <div class="flex flex-col gap-2 py-12 border-b">
+      <div className="flex justify-between">
+        <h4 className="text-2xl font-semibold">{title}</h4>
+        <a
+          href={downloadURL}
+          className="flex justify-center sm:justify-start items-center h-14 sm:w-fit px-8 bg-slate-200 hover:bg-slate-300 text-xl text-black rounded"
+        >
+          {`${buttonText}` + `${downloadType}`}
+        </a>
       </div>
+
+      {isOpen ? (
+        <></>
+      ) : (
+        <div>
+          <a
+            onClick={() => showDetailsHandler()}
+            className="text-blue-700 text-xl underline"
+          >
+            Show checksum
+          </a>
+        </div>
+      )}
 
       <AnimatePresence>
         {isOpen && (
@@ -55,23 +40,24 @@ function DownloadCard(props) {
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "auto" }}
             exit={{ opacity: 0, height: 0 }}
-            className="mt-4"
+            className="flex flex-col gap-4 mt-4"
           >
-            <label>Checksum</label>
-            <div className="p-4 border bg-gray-50 mt-2">
-              <p className="font-mono text-sm break-words">{checksum}</p>
+            <div>
+              <label className="text-sm uppercase text-gray-600">Checksum</label>
+              <div className="p-4 border bg-gray-50">
+                <p className="font-mono text-sm break-words">{checksum}</p>
+              </div>
             </div>
+
+            <a
+              onClick={() => showDetailsHandler()}
+              className="text-blue-700 text-xl underline"
+            >
+              Hide checksum
+            </a>
           </motion.div>
         )}
       </AnimatePresence>
-
-      <a
-        href={downloadURL}
-        download
-        className="flex justify-center sm:justify-start items-center mt-6 h-10 sm:w-fit px-3 bg-blue-700 hover:bg-blue-600 text-sm sm:text-base text-white rounded"
-      >
-        {`${buttonText}` + `${downloadType}`}
-      </a>
     </div>
   );
 }
