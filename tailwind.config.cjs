2aaf51f38f352b28e9fd39b576edcaf085faba31
Dilson's Pickles <a.dawson@mu.se>
2023-08-22 11:43:51 +1000

    Responsive styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		screens: {
+			'xs': '320px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		}
 	},
 	plugins: [],
 }
