35248dbd5f4037735d2f15d828157fa830f48f57
Dilson's Pickles <a.dawson@mu.se>
2023-08-23 16:49:17 +1000

    Adjust responsive behaviour

--- src/components/card/IconLinkCard.jsx
+++ src/components/card/IconLinkCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function IconLinkCard(props) {
   const { icon, title, description, targetURL, linkText } = props;
   return (
-    <div className="col-span-12 sm:col-span-6 flex flex-1 flex-col gap-12 border rounded-lg drop-shadow-lg bg-white p-6 md:p-8 justify-between">
+    <div className="col-span-12 sm:col-span-6 flex flex-1 flex-col gap-12 border rounded-lg drop-shadow-lg bg-white p-6 justify-between">
       <div className="flex flex-col gap-2">
         <span className={`icon icon-large ${icon} text-blue-700`}></span>
         <h4 className="text-xl font-bold">{title}</h4>
