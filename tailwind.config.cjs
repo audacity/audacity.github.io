1d2d943ee9ba15691e6be3656ef2b216851b3c67
Dilson's Pickles <a.dawson@mu.se>
2023-08-24 14:45:40 +1000

    Stop tailwind from injecting base styles twice

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		screens: {
-			'xs': '320px',
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-			'2xl': '1536px',
-		}
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
+};
