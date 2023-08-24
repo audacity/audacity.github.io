5d95a253552d571cf948747bfac632112394a6b4
Dilson's Pickles <a.dawson@mu.se>
2023-08-24 14:45:23 +1000

    Update astro & tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -8,12 +8,18 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://audacity.github.io',
-  integrations: [tailwind(), react()],
+  site: "https://audacity.github.io",
+  integrations: [ tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false,
+  }), react()],
   vite: {
     ssr: {
-      noExternal: ['@datapunt/matomo-tracker-react', '@datapunt/matomo-tracker-js']
-    }
-  }
+      noExternal: [
+        "@datapunt/matomo-tracker-react",
+        "@datapunt/matomo-tracker-js",
+      ],
+    },
+  },
 });
-
