b7a778d18d319fb45c378bdc3bbb81f58b7b2711
Dilson's Pickles <a.dawson@mu.se>
2023-08-29 15:57:10 +1000

    Remove tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://audacity.github.io",
-  integrations: [ tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
-  }), react()],
+  integrations: [ tailwind(), react()],
   vite: {
     ssr: {
       noExternal: [
