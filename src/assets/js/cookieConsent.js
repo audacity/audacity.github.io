f0c5d61f2121c85c7ce654172022806062300d7d
Dilson's Pickles <a.dawson@mu.se>
2023-09-25 16:45:57 +1000

    Add year expiry to cookie

--- src/assets/js/cookieConsent.js
+++ src/assets/js/cookieConsent.js
@@ -1,40 +1,38 @@
 const cookieStorage = {
-    getItem: (key) => {
-      const cookies = document.cookie
-        .split(";")
-        .map((cookie) => cookie.split("="))
-        .reduce(
-          (acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {}
-        );
-      return cookies[key];
-    },
-    setItem: (key, value) => {
-      document.cookie = `${key}=${value}`;
-    },
+  getItem: (key) => {
+    const cookies = document.cookie
+      .split(";")
+      .map((cookie) => cookie.split("="))
+      .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+    return cookies[key];
+  },
+  setItem: (key, value) => {
+    document.cookie = `${key}=${value}; expires=${new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * 365
+    ).toGMTString()}`;
+  },
+};
+
+const storageType = cookieStorage;
+const consentPropertyName = "Audacity_consent";
+
+const showShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+  const consentPopup = document.getElementById("consent-popup");
+  const acceptBtn = document.getElementById("accept");
+
+  const acceptFn = (event) => {
+    saveToStorage(storageType);
+    consentPopup.classList.add("hide");
   };
 
-  const storageType = cookieStorage;
-  const consentPropertyName = "Audacity_consent";
+  acceptBtn.addEventListener("click", acceptFn);
 
-  const showShowPopup = () => !storageType.getItem(consentPropertyName);
-  const saveToStorage = () =>
-    storageType.setItem(consentPropertyName, true);
-
-  window.onload = () => {
-    const consentPopup = document.getElementById('consent-popup');
-    const acceptBtn = document.getElementById('accept');
-
-    const acceptFn = event => {
-      saveToStorage(storageType);
-      consentPopup.classList.add('hide');
-    }
-
-    acceptBtn.addEventListener('click', acceptFn);
-
-    if (showShowPopup(storageType)) {
-      setTimeout(() => {
-        consentPopup.classList.remove('hide')
-      }, 2000);
-     
-    }
-  };
+  if (showShowPopup(storageType)) {
+    setTimeout(() => {
+      consentPopup.classList.remove("hide");
+    }, 2000);
+  }
+};
