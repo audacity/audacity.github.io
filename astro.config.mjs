744f0a9a9025a1ed982c7ccf4a0ddaa8d58ac2c5
Dilson's Pickles <a.dawson@mu.se>
2023-09-26 17:16:52 +1000

    Install astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://audacity.github.io",
-  integrations: [ tailwind({
+  integrations: [tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
-  }), react()],
+    applyBaseStyles: false
+  }), react(), compress()],
   vite: {
     ssr: {
-      noExternal: [
-        "@datapunt/matomo-tracker-react",
-        "@datapunt/matomo-tracker-js",
-      ],
-    },
+      noExternal: ["@datapunt/matomo-tracker-react", "@datapunt/matomo-tracker-js"]
+    }
   },
   redirects: {
     '/3.1.0-video': "https://www.youtube.com/watch?v=HpA138b-J9s",
