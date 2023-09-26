20ac5eb5d694bb89856cae287dbe003a205db977
Dilson's Pickles <a.dawson@mu.se>
2023-09-26 16:40:30 +1000

    Improve accessibility

--- src/components/card/DownloadCard.jsx
+++ src/components/card/DownloadCard.jsx
@@ -6,7 +6,7 @@ const {title, downloadURL, buttonText, downloadType, checksum} = props;
   return (
     <div className="border border-bg-200 rounded-md p-6">
       <div className="flex flex-col sm:flex-row gap-2 justify-between items-center">
-        <h4>{title}</h4>
+        <h2 class="text-xl font-semibold">{title}</h2>
         <a
           href={downloadURL}
           className="flex justify-center text-center items-center px-4 h-12 w-full sm:w-fit bg-slate-200 hover:bg-slate-300 text-base text-black rounded"
@@ -17,8 +17,8 @@ const {title, downloadURL, buttonText, downloadType, checksum} = props;
 
       {checksum && (
         <div className="flex flex-col mt-8 border-t pt-4">
-          <label>Checksum:</label>
-          <div className="mt-2 p-2 bg-gray-50 border border-gray-200">
+          <label for="checksum">Checksum:</label>
+          <div id="checksum" className="mt-2 p-2 bg-gray-50 border border-gray-200">
             <small className="break-words">{checksum}</small>
           </div>
         </div>
