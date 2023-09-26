a21960543484b2016312bc4a8ba2dd138f797982
Dilson's Pickles <a.dawson@mu.se>
2023-09-26 15:45:44 +1000

    Implement "Skip to content"

--- src/components/navigation/NavigationReact.jsx
+++ src/components/navigation/NavigationReact.jsx
@@ -46,7 +46,7 @@ function NavigationReact() {
       <div className="flex max-w-screen-xl mx-auto h-14 px-4 xl:px-0 items-center">
         <div className="flex-1">
           <a className="flex w-fit items-center gap-1 lg:gap-2" href="/">
-            <img className="w-5 lg:w-6 h-full" src={AudacityLogo.src} alt="Audacity logo" />
+            <img className="w-5 lg:w-6 h-full" src={AudacityLogo.src} alt="A yellow and orange waveform between the ears of a set of blue headphones" />
             <brand className="signika text-blue-700 lg:text-lg font-medium lg:leading-none">
               Audacity
             </brand>
