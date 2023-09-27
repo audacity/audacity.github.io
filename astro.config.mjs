6c768e846bd7860a0e5f1c36e288203c6404c4d6
Dilson's Pickles <a.dawson@mu.se>
2023-09-27 13:30:56 +1000

    Install YouTube embed lazy load

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,7 @@ import compress from "astro-compress";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import react from "@astrojs/react";
-
 
 // https://astro.build/config
 export default defineConfig({
