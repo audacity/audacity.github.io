01023f001c134f4e42699ce36e51f7d025f7cbf6
Dilson's Pickles <a.dawson@mu.se>
2023-09-28 11:35:33 +1000

    Update matomo tracking code

--- src/components/card/DownloadCard.jsx
+++ src/components/card/DownloadCard.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
 function DownloadCard(props) {
+  const { title, downloadURL, buttonText, downloadType, checksum } = props;
 
-const {title, downloadURL, buttonText, downloadType, checksum} = props;
+  function handleButtonClick() {
+    _paq.push([
+      "trackEvent",
+      "Button",
+      "Click",
+      `${title + " " + downloadType}`,
+    ]);
+  }
+
   return (
     <div className="border border-bg-200 rounded-md p-6">
       <div className="flex flex-col sm:flex-row gap-2 justify-between items-center">
         <h2 className="text-xl font-semibold">{title}</h2>
         <a
-          onclick="_paq.push(['trackEvent', 'Download', Download button click,'TEST']);"
+          onClick={() => {
+            handleButtonClick();
+          }}
           href={downloadURL}
           className="flex justify-center text-center items-center px-4 h-12 w-full sm:w-fit bg-slate-200 hover:bg-slate-300 text-base text-black rounded"
         >
@@ -19,7 +30,10 @@ const {title, downloadURL, buttonText, downloadType, checksum} = props;
       {checksum && (
         <div className="flex flex-col mt-8 border-t pt-4">
           <label for="checksum">Checksum:</label>
-          <div id="checksum" className="mt-2 p-2 bg-gray-50 border border-gray-200">
+          <div
+            id="checksum"
+            className="mt-2 p-2 bg-gray-50 border border-gray-200"
+          >
             <small className="break-words">{checksum}</small>
           </div>
         </div>
