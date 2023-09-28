f8d8787af41b5d828cb88789f47f95a10ba36ee7
Dilson's Pickles <a.dawson@mu.se>
2023-09-28 11:17:36 +1000

    Update matomo tracking code

--- src/components/card/DownloadCard.jsx
+++ src/components/card/DownloadCard.jsx
@@ -8,7 +8,7 @@ const {title, downloadURL, buttonText, downloadType, checksum} = props;
       <div className="flex flex-col sm:flex-row gap-2 justify-between items-center">
         <h2 className="text-xl font-semibold">{title}</h2>
         <a
-          onClick={`_paq.push(['trackEvent', 'Download', Download button click,'${downloadType}'`}
+          onClick={`_paq.push(['trackEvent', 'Download', Download button click,'${downloadType}'])`}
           href={downloadURL}
           className="flex justify-center text-center items-center px-4 h-12 w-full sm:w-fit bg-slate-200 hover:bg-slate-300 text-base text-black rounded"
         >
