b55d6fe60185a31288f28c23add1ee08a8021363
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2023-10-12 10:41:43 +0200

    2xl size for release vid

--- src/components/homepage/ReleaseVideo.jsx
+++ src/components/homepage/ReleaseVideo.jsx
@@ -12,7 +12,7 @@ function ReleaseVideo() {
   return (
     <section className="bg-gray-900">
       <div className="grid grid-cols-12 max-w-screen-xl mx-auto py-12 gap-y-4">
-        <div className="flex flex-col gap-4 justify-center row-start-2 md:row-start-1 col-start-2 col-span-10 md:col-start-2 md:col-span-3">
+        <div className="flex flex-col gap-4 justify-center row-start-2 md:row-start-1 col-start-2 col-span-10 md:col-start-2 md:col-span-3 2xl:col-start-2 2xl:col-span-4">
           <h2 className="text-white">
             Produce music. Produce podcasts. Take total control of your sound.
           </h2>
@@ -23,7 +23,7 @@ function ReleaseVideo() {
             <p className="dark-hyperlink">More about our new release</p>
           </a>
         </div>
-        <div className="flex align-middle col-start-2 col-span-10 md:col-start-6 md:col-span-6">
+        <div className="flex align-middle col-start-2 col-span-10 md:col-start-6 md:col-span-6 2xl:col-start-7 2xl:col-span-7">
           {isClicked ? (
             <iframe
               className="w-full aspect-video rounded-md shadow-xl"
