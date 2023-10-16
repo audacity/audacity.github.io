0d8f703e080821f0e128c751ffd5eed476d794d9
Dilson's Pickles <a.dawson@mu.se>
2023-10-16 11:46:25 +1100

    Remove possible Matomo tracking code duplication

--- src/assets/js/cookieConsent.js
+++ src/assets/js/cookieConsent.js
@@ -39,20 +39,20 @@ const applyMatomoTrackingCode = () => {
   })();
 };
 
-const applyMatomoTrackingCodeWithoutCookies = () => {
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["disableCookies"]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://matomo.audacityteam.org/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-};
+// const applyMatomoTrackingCodeWithoutCookies = () => {
+//   var _paq = window._paq = window._paq || [];
+//   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+//   _paq.push(["disableCookies"]);
+//   _paq.push(['trackPageView']);
+//   _paq.push(['enableLinkTracking']);
+//   (function() {
+//     var u="https://matomo.audacityteam.org/";
+//     _paq.push(['setTrackerUrl', u+'matomo.php']);
+//     _paq.push(['setSiteId', '2']);
+//     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+//     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+//   })();
+// };
 
 window.onload = () => {
   const consentPopup = document.getElementById("consent-popup");
@@ -68,7 +68,7 @@ window.onload = () => {
   const rejectCookie = (event) => {
     saveRejectToStorage(storageType);
     consentPopup.classList.add("hide");
-    applyMatomoTrackingCodeWithoutCookies();
+    // applyMatomoTrackingCodeWithoutCookies();
   };
 
   acceptBtn.addEventListener("click", acceptCookie);
