5f2906ab504ac584b1c4b40732c833b57743707f
Dilson's Pickles <a.dawson@mu.se>
2023-10-17 14:38:03 +1100

    Install astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     applyBaseStyles: false
-  }), react(), sitemap()],
+  }), react(), sitemap(), compressor()],
   vite: {
     ssr: {
       noExternal: ["@datapunt/matomo-tracker-react", "@datapunt/matomo-tracker-js"]
