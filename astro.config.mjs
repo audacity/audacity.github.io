e57e65c2a0b26511b5ed40a4d3680049141749fa
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2023-10-22 10:14:05 +0200

    make cla link case insensitive

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,6 +64,7 @@ export default defineConfig({
     "/community/developers/": "https://audacity.gitbook.io/dev",
     "/community/translators/": "https://support.audacityteam.org/community/contributing/translating",
     "/copyright/": "https://github.com/audacity/audacity/blob/master/LICENSE.txt",
-    "/Setup-Exit-Codes/": "https://support.audacityteam.org/troubleshooting/error-codes/installation-exit-codes"
+    "/Setup-Exit-Codes/": "https://support.audacityteam.org/troubleshooting/error-codes/installation-exit-codes",
+    "/cla": "/CLA"
   }
 });
