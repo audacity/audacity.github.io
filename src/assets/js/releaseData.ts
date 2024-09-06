5b10db244d3c36babd1b00d0f0e27944ae220f69
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2024-09-06 20:00:12 +0200

    update openVINO

--- src/assets/js/releaseData.ts
+++ src/assets/js/releaseData.ts
@@ -216,12 +216,12 @@ type OpenvinoReleaseDirectory = {
 };
 
 export const openvinoReleases: OpenvinoReleaseDirectory = {
-  version: "3.6.1",
-  revision: "R3.1",
+  version: "3.6.2",
+  revision: "R3.2",
   win: [
     {
       name: "OpenVino AI Plugins",
-      browser_download_url: "https://github.com/intel/openvino-plugins-ai-audacity/releases/download/v3.6.1-R3.1/audacity-win-v3.6.1-R3.1-64bit-OpenVINO-AI-Plugins.exe",
+      browser_download_url: "https://github.com/intel/openvino-plugins-ai-audacity/releases/download/v3.6.2-R3.2/audacity-win-v3.6.2-R3.2-64bit-OpenVINO-AI-Plugins.exe",
       type: ".exe"
     },
   ],
