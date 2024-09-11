e6aeacc6e98887ee9c90d8be4f41bd0ba64a78b3
Dilson's Pickles <a.dawson@mu.se>
2024-09-11 10:48:01 +1000

    Format button

--- src/components/button/JoinAudioDotComButton.jsx
+++ src/components/button/JoinAudioDotComButton.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function handleButtonClick() {
-    if (typeof _paq !== "undefined") {
-      _paq.push([
-        "trackEvent",
-        "CTA Button",
-        "audio.com CTA",
-        "audio.com block CTA",
-      ]);
-    }
+  if (typeof _paq !== "undefined") {
+    _paq.push([
+      "trackEvent",
+      "CTA Button",
+      "audio.com CTA",
+      "audio.com block CTA",
+    ]);
   }
+}
 
 function JoinAudioDotComButton() {
   return (
     <a
-            onClick={() => {
-              handleButtonClick();
-            }}
-            href="https://audio.com/auth/sign-up?mtm_campaign=audacityteamorg&mtm_content=Block_button"
-            className="px-6 py-4 bg-blue-700 w-fit text-white rounded hover:bg-blue-600"
-          >
-            Join for free
-          </a>
-  )
+      onClick={() => {
+        handleButtonClick();
+      }}
+      href="https://audio.com/auth/sign-up?mtm_campaign=audacityteamorg&mtm_content=Block_button"
+      className="px-6 py-4 bg-blue-700 w-fit text-white rounded hover:bg-blue-600"
+    >
+      Join for free
+    </a>
+  );
 }
 
-export default JoinAudioDotComButton
+export default JoinAudioDotComButton;
