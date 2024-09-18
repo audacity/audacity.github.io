0ca847bf3d0837bb35e9b517bb376f4f6be2e65d
Dilson's Pickles <a.dawson@mu.se>
2024-09-18 19:37:58 +1000

    Update card height styles

--- src/components/card/FeatureCard.jsx
+++ src/components/card/FeatureCard.jsx
@@ -4,7 +4,7 @@ function FeatureCard(props) {
   const { icon, title, description } = props;
 
   return (
-    <div className="h-full col-span-6 xl:col-span-3 p-4 md:p-6 border drop-shadow-sm md:drop-shadow-lg bg-white rounded-lg flex flex-col md:gap-2">
+    <div className="min-h-72 col-span-6 xl:col-span-3 p-4 md:p-6 border drop-shadow-sm md:drop-shadow-lg bg-white rounded-lg flex flex-col md:gap-2">
       <span className={`icon icon-medium text-blue-700 ${icon}`}></span>
       <p className="text-lg font-semibold">{title}</p>
       <p className="">{description}</p>
