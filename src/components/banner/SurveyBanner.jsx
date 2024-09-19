21f7d47da5f9bb05203315db4d9b2be80fc6716e
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2024-09-19 14:51:57 +0200

    survey is live

--- src/components/banner/SurveyBanner.jsx
+++ src/components/banner/SurveyBanner.jsx
@@ -3,7 +3,7 @@ import "../../styles/icons.css";
 
 function SurveyBanner(url) {
   //no survey going on at the moment
-  return null;
+  //return null;
 
   function handleButtonClick() {
     if (typeof _paq !== "undefined") {
@@ -23,14 +23,14 @@ function SurveyBanner(url) {
     >
       <div className="flex gap-2 flex-wrap my-4 mx-2">
         <p className="text-lg font-bold text-gray-900">
-          1-2 minute survey:
+          3 minute survey:
         </p>
         <p className="text-lg text-gray-900">
-          Help us understand why and how people are using AI tools for audio editing, VO and production work.
+          Help us understand what sort of plugins and features you'd be interested in next.
         </p>
       </div>
       <a
-        href="https://xo9ucdlwlod.typeform.com/to/LPNgta5L"
+        href="https://po415hli6k2.typeform.com/hub-audacity"
         id="survey-button"
         onClick={() => {handleButtonClick();}}
         className="flex text-lg h-12 my-4 justify-center items-center px-4 border-2 border-gray-900 rounded-md hover:bg-gray-900 hover:text-white"
