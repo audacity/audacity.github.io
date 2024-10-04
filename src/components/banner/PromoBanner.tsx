8d6e342748df6ead0a7e93c1e044dda7738800c7
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2024-10-04 16:04:26 +0200

    conditional banner

--- src/components/banner/PromoBanner.tsx
+++ src/components/banner/PromoBanner.tsx
@@ -1,4 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { museHubReleases, audacityReleases } from "../../assets/js/releaseData";
 import "../../styles/icons.css";
+import DownloadMuseHubButton from "../button/DownloadMuseHubButton";
+import platform from "platform";
 declare interface Props {
   url: string;
 }
@@ -6,6 +10,18 @@ declare interface Props {
 function PromoBanner({ url }: Props) {
   //no promo running
   //return null;
+  const [showBanner, setShowBanner] = useState(true);
+
+  function getHref() {
+    switch(platform.os.family){
+      case "OS X":
+        return museHubReleases.mac[0].browser_download_url;
+      case "Windows":
+        return museHubReleases.win[0].browser_download_url;
+      default: 
+        setShowBanner(false); return "#";
+    }
+  }
 
   function handleButtonClick() {
     if (typeof _paq !== "undefined") {
@@ -13,12 +29,12 @@ function PromoBanner({ url }: Props) {
         "trackEvent",
         "Promo CTA",
         "Promo CTA button",
-        "Go to Survey",
+        "Muse hub promo CTA",
       ]);
     }
   }
 
-  return (
+  return ( <> { showBanner &&
       <div
         id="promo-banner"
         className="flex items-center justify-center min-h-24 bg-yellow-300 gap-4 flex-wrap"
@@ -29,8 +45,9 @@ function PromoBanner({ url }: Props) {
             Polyspectral MBC Multiband Compressor! Now $9.99🔥
           </p>
         </div>
+        
         <a
-          href="https://musehub.com/?mtm_campaign=audacityteam&mtm_content=bannerad"
+          href={getHref()}
           id="promo-button"
           onClick={handleButtonClick}
           className="flex text-lg font-bold h-12 my-4 justify-center items-center px-4 border-2 border-gray-900 rounded-md hover:bg-gray-900 hover:text-white"
@@ -38,6 +55,7 @@ function PromoBanner({ url }: Props) {
           Get it on the Muse Hub
         </a>
       </div>
+    } </>
   );
 }
 
