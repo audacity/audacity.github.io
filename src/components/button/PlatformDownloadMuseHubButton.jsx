1899d0e0e020f7e8285057c93372f3bea280cd09
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2025-02-11 11:04:44 +0100

    debug done

--- src/components/button/PlatformDownloadMuseHubButton.jsx
+++ src/components/button/PlatformDownloadMuseHubButton.jsx
@@ -4,10 +4,8 @@ import useBrowserOS from "../../hooks/useDetectOS";
 function PlatformDownloadMuseHubButton(props) {
   const { museHubReleaseData } = props;
   const browserOS = useBrowserOS();
-  console.log("browserOS", browserOS);
 
   function onClickButtonHandler() {
-    console.log("buttonhandler", browserOS);
     if (typeof _paq !== "undefined") {
       _paq.push([
         "trackEvent",
