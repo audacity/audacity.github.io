adfd2c9f4ddeb794c096c0dd4b19a4004919efe0
Dilson's Pickles <a.dawson@mu.se>
2025-03-18 12:03:34 +1100

    Try another method

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,17 +10,25 @@ import compressor from "astro-compressor";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.audacityteam.org",
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }), react(), sitemap(), compressor()],
+  integrations: [
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+    compressor(),
+  ],
   vite: {
     define: {
       "import.meta.env.BRANCH": JSON.stringify(process.env.BRANCH),
     },
     ssr: {
-      noExternal: ["@datapunt/matomo-tracker-react", "@datapunt/matomo-tracker-js"]
-    }
+      noExternal: [
+        "@datapunt/matomo-tracker-react",
+        "@datapunt/matomo-tracker-js",
+      ],
+    },
   },
 });
