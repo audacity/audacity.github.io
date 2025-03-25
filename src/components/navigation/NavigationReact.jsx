2a954c725aea177025ea5c2a2d7d1053f024676b
Dilson's Pickles <a.dawson@mu.se>
2025-03-25 14:35:32 +1100

    Optimise brand logo

--- src/components/navigation/NavigationReact.jsx
+++ src/components/navigation/NavigationReact.jsx
@@ -71,6 +71,8 @@ function NavigationReact(props) {
               className="w-5 lg:w-6 h-full"
               src={AudacityLogo.src}
               alt="A yellow and orange waveform between the ears of a set of blue headphones"
+              width="24"
+              height="24"
             />
             <p className="signika text-blue-700 lg:text-lg font-medium lg:leading-none">
               Audacity
@@ -113,6 +115,9 @@ function NavigationReact(props) {
               className="fill-gray-700 w-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 320 512"
+              width="16" 
+              height="16"
+              aria-hidden="true"
             >
               <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
             </svg>
@@ -121,6 +126,9 @@ function NavigationReact(props) {
               className="fill-gray-700 w-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
+              width="16" 
+              height="16"
+              aria-hidden="true"
             >
               <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
             </svg>
