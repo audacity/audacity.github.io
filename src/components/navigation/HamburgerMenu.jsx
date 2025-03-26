0ff46ca91e51d9430318ad3e05827f7fc3af7e5e
Dilson's Pickles <a.dawson@mu.se>
2025-03-26 14:14:45 +1100

    Remove animation from hamburger menu

--- src/components/navigation/HamburgerMenu.jsx
+++ src/components/navigation/HamburgerMenu.jsx
@@ -3,16 +3,15 @@ import React from "react";
 function HamburgerMenu(props) {
   const { navLinks, onLinkClick } = props;
   return (
-    <div className="flex flex-col py-2 bg-gray-50 border-y-2 absolute left-0 right-0 z-40 animate-fadeIn">
+    <div className="flex flex-col py-2 bg-gray-50 border-y-2 absolute left-0 right-0 z-40">
       {navLinks.map((navLink, index) => {
         return (
           <a 
             key={index} 
             onClick={() => onLinkClick()} 
-            className="py-3 px-4 text-gray-800 hover:text-blue-700 hover:bg-gray-100 animate-fadeInSlow" 
+            className="py-3 px-4 text-gray-800 hover:text-blue-700 hover:bg-gray-100"
             href={navLink.href} 
             target={navLink.target}
-            style={{ animationDelay: `${index * 50}ms` }}
           >
             {navLink.linkText}
           </a>
