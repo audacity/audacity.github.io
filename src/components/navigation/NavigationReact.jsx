431419e0b32e4b21ca6f272ce841b2c3bf1641bb
Dilson's Pickles <a.dawson@mu.se>
2025-03-26 13:53:53 +1100

    Tidy up navbar element

--- src/components/navigation/NavigationReact.jsx
+++ src/components/navigation/NavigationReact.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { AnimatePresence } from "framer-motion";
-import HamburgerMenu from "./HamburgerMenu";
 import AudacityLogo from "../../assets/img/Audacity_Logo.svg";
 import "@fontsource-variable/signika";
 import "../../styles/fonts.css";
@@ -63,7 +61,7 @@ function NavigationReact(props) {
   }
 
   return (
-    <nav className="border-b-2 z-3 filter bg-white bg-opacity-90 backdrop-blur-xl ">
+    <nav className="border-b-2 z-3 filter bg-white bg-opacity-90 backdrop-blur-xl">
       <div className="flex h-14 items-center max-w-screen-2xl mx-auto px-4 md:px-6">
         <div className="flex-1">
           <a className="flex w-fit items-center gap-1 lg:gap-2" href="/">
@@ -71,8 +69,6 @@ function NavigationReact(props) {
               className="w-5 lg:w-6 h-full"
               src={AudacityLogo.src}
               alt="A yellow and orange waveform between the ears of a set of blue headphones"
-              width="24"
-              height="24"
             />
             <p className="signika text-blue-700 lg:text-lg font-medium lg:leading-none">
               Audacity
@@ -115,9 +111,6 @@ function NavigationReact(props) {
               className="fill-gray-700 w-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 320 512"
-              width="16" 
-              height="16"
-              aria-hidden="true"
             >
               <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
             </svg>
@@ -126,23 +119,28 @@ function NavigationReact(props) {
               className="fill-gray-700 w-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
-              width="16" 
-              height="16"
-              aria-hidden="true"
             >
               <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
             </svg>
           )}
         </button>
       </div>
-      <AnimatePresence>
-        {isHamburgerMenuOpen && (
-          <HamburgerMenu
-            navLinks={navLinks}
-            onLinkClick={handleHamburgerMenuClick}
-          />
-        )}
-      </AnimatePresence>
+
+      {isHamburgerMenuOpen && (
+        <div className="flex flex-col py-2 bg-gray-50 border-y-2 absolute left-0 right-0 z-40 hamburger-menu">
+          {navLinks.map((navLink, index) => (
+            <a
+              key={index}
+              onClick={() => handleHamburgerMenuClick()}
+              className="py-3 px-4 text-gray-800 hover:text-blue-700 hover:bg-gray-100"
+              href={navLink.href}
+              target={navLink.target}
+            >
+              {navLink.linkText}
+            </a>
+          ))}
+        </div>
+      )}
     </nav>
   );
 }
