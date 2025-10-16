4b3c9b57f493e0a19f22e4ba908b596fcba26e1e
Mudskipper <a.dawson@mu.se>
2025-10-16 14:11:31 +1100

    Update FeatureCards section with new branding
    - Add bg-background-medium background
    - Change icon color from blue-700 to accent

--- src/components/card/FeatureCard.jsx
+++ src/components/card/FeatureCard.jsx
@@ -5,7 +5,7 @@ function FeatureCard(props) {
 
   return (
     <div className="min-h-72 col-span-1 xl:col-span-1 p-4 md:p-6 border drop-shadow-sm md:drop-shadow-lg bg-white rounded-lg flex flex-col md:gap-2">
-      <span className={`icon icon-medium text-blue-700 ${icon}`}></span>
+      <span className={`icon icon-medium text-accent ${icon}`}></span>
       <p className="text-lg font-semibold">{title}</p>
       <p className="">{description}</p>
     </div>
