36589e1d3d028be1cfef4a4dea736aac76c728e2
Mudskipper <a.dawson@mu.se>
2025-10-20 16:21:29 +1100

    fix: restore wordmark size to match icon
    Icon: h-5 (20px) - smaller
    Wordmark: h-5 (20px) - original size
    Added: 'by muse group' text in 14px

--- src/components/navigation/NavigationReact.jsx
+++ src/components/navigation/NavigationReact.jsx
@@ -73,7 +73,7 @@ function NavigationReact(props) {
         <div className="flex-1">
           <a className="flex w-fit items-center gap-2" href="/">
             <AudacityIconSVG className="h-5 fill-text-primary" />
-            <AudacityWordmarkSVG className="h-4 fill-text-primary" />
+            <AudacityWordmarkSVG className="h-5 fill-text-primary" />
             <span className="font-muse-sans text-14 text-text-primary">by muse group</span>
           </a>
         </div>
