971e2fd36efcf8601098a44d6aed7121c125d6cb
Teetow <teetow@gmail.com>
2026-02-19 17:51:21 +0100

    chore(tooling): add bun typings and astro prettier plugin

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
+/// <reference types="bun" />
 
 interface ImportMetaEnv {
-	readonly NETLIFY_CONTEXT?: string;
+  readonly NETLIFY_CONTEXT?: string;
 }
